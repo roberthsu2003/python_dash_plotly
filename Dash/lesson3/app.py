8df545128132defb0569982b32c4b497fd2475a8
roberthsu <roberthsu2003@gmail.com>
2022-05-05 08:39:24 +0800

    modify

--- Dash/lesson3/app.py
+++ Dash/lesson3/app.py
@@ -2,7 +2,8 @@ import dash
 from dash import html
 import dash_bootstrap_components as dbc
 
-app = dash.Dash(__name__,external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = dash.Dash(__name__,
+                external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.layout = html.Div([
     html.H1(children='Poverty And Equitey Database',
             style={
