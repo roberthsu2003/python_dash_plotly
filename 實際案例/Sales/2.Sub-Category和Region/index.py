af5fbcdda7fb82c89ded520541fef17be7995cdc
徐國堂 <roberthsu2003@gmail.com>
2022-06-13 14:13:49 +0800

    modify

--- 實際案例/Sales/2.Sub-Category和Region/index.py
+++ 實際案例/Sales/2.Sub-Category和Region/index.py
@@ -46,7 +46,7 @@ app.layout = dbc.Container([
     ]),
     dbc.Row([
             dbc.Col([
-                html.H5('2018子類別銷售',className='mb-3'),
+                html.H5('子類別或地區',className='mb-3'),
                 dcc.RadioItems(
                     ["子類別","地區"],
                     id='sub_segment',
@@ -74,6 +74,8 @@ app.layout = dbc.Container([
 def update_barChart(select_years,sub_category,sub_segment):
     sales1 = sales.groupby(['Year', '子類別', '部分'])['Sales'].sum().reset_index()
     sales2 = sales1[(sales1['Year'] == select_years) & (sales1['部分'] == sub_category)].sort_values(by='Sales',ascending=False).nlargest(5,columns=['Sales'])
+    sales3 = sales.groupby(['Year', '地區', '部分'])['Sales'].sum().reset_index()
+    sales4 = sales3[(sales3['Year'] == select_years) & (sales3['部分'] == sub_category)].sort_values(by='Sales', ascending=False)
     fig = go.Figure()
     if sub_segment == '子類別':
         fig.add_trace(
@@ -94,11 +96,86 @@ def update_barChart(select_years,sub_category,sub_segment):
             )
         )
         fig.update_layout(
+            title={
+                'text': str(select_years) + sub_category +'子類別銷售金額',
+                'x':0,
+                'y':0.9
+            },
+            title_font={
+                'color':'#0f5132',
+                'size':20
+            },
+            hovermode='closest',
             paper_bgcolor="#fff",
             plot_bgcolor="#fff",
+            margin_l=0,
+            margin_r=0
         )
+        fig.update_xaxes(
+            dict(
+                title='<b>金額</b>',
+                ticks='outside',
+                showgrid=False
+            )
+        )
+        fig.update_yaxes(
+            dict(
+                title='<b>子類別</b>',
+                ticks='outside',
+                showgrid=False
+            )
+        )
+
         return fig
-    else:
+    elif(sub_segment == '地區'):
+        fig.add_trace(
+            go.Bar(
+                x=sales4['Sales'],
+                y=sales4['地區'],
+                text=sales4['地區'],
+                texttemplate=[f'{x:,.0f}美金' for x in sales4['Sales']],
+                textposition='auto',
+                orientation='h',
+                marker=dict(color='#19AAE1'),
+                hoverinfo='text',
+                hovertext=
+                '<b>年度</b>' + sales4['Year'].astype(str) + '<br>' +
+                '<b>區域</b>' + sales4['部分'].astype(str) + '<br>' +
+                '<b>地區</b>' + sales4['地區'].astype(str) + '<br>' +
+                '<b>銷售額</b>' + [f'{x:,.0f}美金' for x in sales4['Sales']] + '<br>'
+            )
+        )
+        fig.update_layout(
+            title={
+                'text': str(select_years) + sub_category + '地區銷售金額',
+                'x': 0,
+                'y': 0.9
+            },
+            title_font={
+                'color': '#0f5132',
+                'size': 20
+            },
+            hovermode='closest',
+            paper_bgcolor="#fff",
+            plot_bgcolor="#fff",
+            margin_l=0,
+            margin_r=0
+        )
+        fig.update_xaxes(
+            dict(
+                title='<b>金額</b>',
+                ticks='outside',
+                showgrid=False
+            )
+        )
+        fig.update_yaxes(
+            dict(
+                title='<b>地區</b>',
+                ticks='outside',
+                showgrid=False
+            )
+        )
+
         return fig
 
 if __name__ == "__main__":
