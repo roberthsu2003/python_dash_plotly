368791607b1c702818a0d0961dd43570c9489b70
徐國堂 <roberthsu2003@gmail.com>
2022-06-16 15:04:20 +0800

    modify

--- 實際案例/Sales/4line_chart/index.py
+++ 實際案例/Sales/4line_chart/index.py
@@ -7,7 +7,7 @@ import dash_bootstrap_components as dbc
 sales = pd.read_csv('train2.csv')
 sales['Order Date'] = pd.to_datetime(sales['Order Date'],format='%d/%m/%Y')
 sales['Year'] = sales['Order Date'].dt.year
-sales['Month'] = sales['Order Date'].dt.month_name()
+sales['Month'] = sales['Order Date'].dt.month
 segment = sales['部分'].unique()
 
 app = Dash(__name__,external_stylesheets=[dbc.themes.BOOTSTRAP])
@@ -64,7 +64,12 @@ app.layout = dbc.Container([
             dbc.Col([
                 dcc.Graph(id='line_chart')
             ], width=4),
-            dbc.Col("third", width=2)
+            dbc.Col([
+                html.P('年度銷售額',className='pt-3 fs-4 mb-4'),
+                html.Div(id='text1',className='mb-4'),
+                html.Div(id='text2',className='mb-4'),
+                html.Div(id='text3',className='mb-4')
+            ], width=2,className='bg-light text-center shadow rounded')
     ],className='pt-5'),
     
 ],class_name="py-5")
@@ -250,7 +255,9 @@ def update_donut_chart(select_years, sub_category):
 
 def update_line_chart(select_years, sub_category):
     sales6 = sales.groupby(['Year', 'Month', '部分'])['Sales'].sum().reset_index()
-    sales7 = sales6[(sales6['Year'] == select_years) & (sales6['部分'] == sub_category)]
+    sales7 = sales6[(sales6['Year']==select_years) & (sales6['部分']==sub_category)].sort_values(by='Month')
+
+
     fig = go.Figure()
     fig.add_trace(
         go.Scatter(
@@ -259,6 +266,7 @@ def update_line_chart(select_years, sub_category):
             text = sales7['Sales'],
             texttemplate='$' + '%{text:,.2s}',
             textposition='bottom left',
+            mode='markers+lines+text',
             line=dict(
                 width=3,
                 color='#FF00FF'
@@ -283,7 +291,7 @@ def update_line_chart(select_years, sub_category):
 
     fig.update_layout(
         title = {
-            'text': str(select_years) + ' ' +str(sub_category)+'<br>'+ '類別銷售比例',
+            'text': str(select_years) + ' ' +str(sub_category)+'<br>'+ '每月銷售額圖表',
             'y':0.95,
             'x':0.5,
             'xanchor':'center',
@@ -293,7 +301,10 @@ def update_line_chart(select_years, sub_category):
             'color':'#0f5132',
             'size':20
         },
+        hovermode='closest',
         showlegend=False,
+        paper_bgcolor='#ffffff',
+        plot_bgcolor='#ffffff',
         margin=dict(
             l = 0,
             r = 0,
@@ -302,6 +313,93 @@ def update_line_chart(select_years, sub_category):
         height = 500,
     )
 
+    fig.update_xaxes(
+        dict(
+            title="<b>月份</b>",
+            color='#333333',
+            showline=True,
+            showgrid=False,
+            linecolor='orange',
+            linewidth=1,
+            tickmode='linear',
+            ticks='outside',
+            tickfont=dict(
+                family='Aerial',
+                color='#333333',
+                size=12
+            )
+        )
+    )
+
+    fig.update_yaxes(
+        title='<b>銷售金額</b>',
+        color='#333333',
+        showline=False,
+        showgrid=True,
+        showticklabels=True,
+        linewidth=1,
+        ticks='outside',
+        tickfont=dict(
+           family='Aerial',
+            color='#333333',
+            size=12
+        )
+    )
+
     return fig
+@app.callback(
+    Output('text1','children'),
+    Input('select_years','value')
+)
+def current_year(select_years):
+    sales8 = sales.groupby(['Year'])['Sales'].sum().reset_index()
+    current_year = sales8[sales8['Year']==select_years]['Sales'].sum()
+    content1 = [
+        html.P(
+        f'{select_years}總銷售額'
+        ,className='fs-5 mb-1'),
+        html.P(
+            f'${current_year:,.0f}',
+        className='fs-4 fw-bold text-primary')
+    ]
+    return content1
+
+@app.callback(
+    Output('text2','children'),
+    Input('select_years','value')
+)
+def previous_year(select_years):
+    sales9 = sales.groupby(['Year'])['Sales'].sum().reset_index()
+    sales9['py'] = sales9['Sales'].shift(1)
+    previous_year = sales9[sales9['Year'] == select_years]['py'].sum()
+    content2 = [
+        html.P(
+        f'{select_years-1}總銷售額'
+        ,className='fs-5 mb-1'),
+        html.P(
+            f'${previous_year:,.0f}',
+        className='fs-4 fw-bold text-primary')
+    ]
+    return content2
+
+@app.callback(
+    Output('text3','children'),
+    Input('select_years','value')
+)
+def yoy_growth(select_years):
+    sales10 = sales.groupby(['Year'])['Sales'].sum().reset_index()
+    sales10['yoy growth'] = sales10['Sales'].pct_change()
+    sales10['yoy growth'] = sales10['yoy growth'] * 100
+    yoy_growth = sales10[sales10['Year'] == select_years]['yoy growth'].sum()
+    content3 = [
+        html.P(
+        f'年成長比例',
+        className='fs-5 mb-1'),
+        html.P(
+            f'{yoy_growth:,.2f}%',
+        className='fs-4 fw-bold text-danger')
+    ]
+    return content3
+
 if __name__ == "__main__":
     app.run_server(debug=True)
